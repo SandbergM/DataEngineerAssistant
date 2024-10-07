ed7a12ff75d43823373f63e67451f79904aff0f9
SandbergM <marcus.a.sandberg@outlook.com>
2024-10-07 21:50:53 +0200

    no msg

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-
-
-import pandas as pd
-
-# Access environment variables
-df = pd.read_csv('./data/Electric_Vehicle_Population_Data.csv')
-
-
 from Assistent import Assistent
-
 Assistent().make_alive()
