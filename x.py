5b155ef36db0820d6e97d503df98ec40f658485f
SandbergM <56336779+SandbergM@users.noreply.github.com>
2024-12-06 21:12:18 +0100

    Update x.py

--- x.py
+++ x.py
@@ -1,117 +1,70 @@
 def read_data(file_name):
-    with open(f"{file_name}.txt", 'r') as file:
-        content = file.read()
-        data = content.split('\n')
-        return data
+    with open(f'{file_name}.txt') as f:
+        return [list(x) for x in f.read().split("\n")]
 
-def is_diagonal(data, idx, jdx):
+import time
 
-    cords = [
-        [
-            [idx, idx-1, idx-2, idx-3],
-            [jdx, jdx-1, jdx-2, jdx-3]
-        ],
-        [
-            [idx, idx-1, idx-2, idx-3],
-            [jdx, jdx+1, jdx+2, jdx+3]
-        ],
-    ]
+data = read_data("data")
 
-    ans = 0
-    for cord in cords:
+def run_this_stupid_fucking_puzzle(data):
 
-        conditions = [
-            min(cord[0]) >= 0,
-            min(cord[1]) >= 0,
-            max(cord[0]) < len(data),
-            max(cord[1]) < len(data[idx])
-        ]
+    steps = {   
+        "^" : (-1, 0),
+        ">" : (0, 1),
+        "v" : (1, 0),
+        "<" : (0, -1)
+    }
 
-        if all(conditions):
-            s = "".join([
-                data[cord[0][0]][cord[1][0]],
-                data[cord[0][1]][cord[1][1]],
-                data[cord[0][2]][cord[1][2]],
-                data[cord[0][3]][cord[1][3]]
-            ])
+    directions = {
+        "^" : ">",
+        ">" : "v",
+        "v" : "<",
+        "<" : "^"
+    }
 
-            if "XMAS" in [s, s[::-1]]:
-                ans +=1
-    return ans
+    __idx = max([idx if "^" in data[idx] else 0 for idx in range(len(data))])
+    __jdx = data[__idx].index("^")
 
-
-def is_horizontal(data, idx, jdx):
-    
-        cords_a = [idx, idx, idx, idx]
-        cords_b = [jdx, jdx-1, jdx-2, jdx-3]
-    
-        ans = 0
-    
-        if jdx-3 >= 0:
-            s = "".join([
-                data[cords_a[0]][cords_b[0]],
-                data[cords_a[1]][cords_b[1]],
-                data[cords_a[2]][cords_b[2]],
-                data[cords_a[3]][cords_b[3]]
-            ])
-    
-            if "XMAS" in [s, s[::-1]]:
-                ans +=1
-    
-        return ans
-
-
-def is_vertical(data, idx, jdx):
-        
-    cords_a = [idx, idx-1, idx-2, idx-3]
-    cords_b = [jdx, jdx, jdx, jdx]
+    direction = data[__idx][__jdx]
 
     ans = 0
 
-    if idx-3 >= 0:
-        s = "".join([
-            data[cords_a[0]][cords_b[0]],
-            data[cords_a[1]][cords_b[1]],
-            data[cords_a[2]][cords_b[2]],
-            data[cords_a[3]][cords_b[3]]
-        ])
+    for i in range(len(data)):
 
-        if "XMAS" in [s, s[::-1]]:
-            ans +=1
+        for j in range(len(data[i])):
 
-    return ans
+            data_copy = read_data("data")
 
+            _direction = direction
+            data_copy[i][j] = "O"
 
-def part_2(data):
+            idx = __idx
+            jdx = __jdx
 
-    ans = 0
+            while True:
 
-    def safe_read(data, idx, jdx):
-        try:
-            return data[idx][jdx]
-        except:
-            return ""
+                _i, _j = idx, jdx
 
-    for idx in range(len(data)):
-        for jdx in range(len(data[idx])):
-            if data[idx][jdx] == "A":
+                idx += steps[_direction][0]
+                jdx += steps[_direction][1]
 
-                cross_a = "".join([
-                    safe_read(data, idx-1, jdx-1),
-                    "A",
-                    safe_read(data, idx+1, jdx+1),
-                ])
+                if idx < 0 or idx >= len(data_copy) or jdx < 0 or jdx >= len(data_copy[idx]):
+                    break
 
-                cross_b = "".join([
-                    safe_read(data, idx-1, jdx+1),
-                    "A",
-                    safe_read(data, idx+1, jdx-1),
-                ])
+                if data_copy[idx][jdx] in ["#", "O"]:
+                    _direction = directions[_direction]
+                    idx, jdx = _i, _j
+                    continue
 
-                ans += all([ cross_a in ["MAS", "SAM"], cross_b in ["MAS", "SAM"] ])
+                if data_copy[idx][jdx] == _direction:
+                    ans += 1
+                    break
+
+                data_copy[idx][jdx] = _direction
 
     return ans
-
-
-print("Example 2 : ", part_2(read_data("example")))
-print("Part 2 : ", part_2(read_data("data")))
+    
+start_time = time.time()
+print(run_this_stupid_fucking_puzzle(data))
+end_time = time.time()
+print(f"Execution time: {(end_time - start_time) * 1000:.2f} ms")
